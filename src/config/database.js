aa71b58834210650fc8a87f52ce4b56e8d0ca6a2
unknown <pmmike2106@gmail.com>
2025-02-18 11:55:34 +0530

    fixed .env  badAuth error

--- src/config/database.js
+++ src/config/database.js
@@ -6,17 +6,15 @@ const USERNAME = process.env.USERNAME;
 const PASSWORD = process.env.PASSWORD;
 const DB_NAME = process.env.DB_NAME;
 const CLUSTER_NAME = process.env.CLUSTER_NAME;
+console.log(USERNAME, PASSWORD, DB_NAME, CLUSTER_NAME);
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(`mongodb+srv://${USERNAME}:${PASSWORD}@${CLUSTER_NAME}/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(`mongodb+srv://pmmike2106:${PASSWORD}@cluster0.qwdyt.mongodb.net/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`);
     console.log("Database connected");
   } catch (error) {
     console.error("Database connection error:", error);
-    process.exit(1); // Exit the process with failure
+   
   }
 };
 
